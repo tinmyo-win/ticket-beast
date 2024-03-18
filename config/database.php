388ac81579cefcc2093047e487ca6ea24837e053
Tryon <tryon@visibleone.com>
2024-03-19 03:17:08 +0630

    change db for singlestore

--- config/database.php
+++ config/database.php
@@ -109,7 +109,7 @@ return [
             'strict' => true,
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+                PDO::MYSQL_ATTR_SSL_CA => storage_path(env('MYSQL_ATTR_SSL_CA')),
                 PDO::ATTR_EMULATE_PREPARES => true,
                 PDO::ATTR_PERSISTENT => true
             ]) : [],
